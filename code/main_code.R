d9328b99f3dd2257cb16087eb782e605bc493b86
majimaken <kengeeler@gmail.com>
2021-05-10 21:39:22 +0200

    Finished data exploration

--- code/main_code.R
+++ code/main_code.R
@@ -17,14 +17,29 @@ tail(ETH)
 
 logret <- diff(log(ETH$`ETH-USD.Close`))
 logret <- na.omit(logret)
-colnames(logret) <- "ETH Logarithmic Returns"
+colnames(logret) <- "ETH Log Returns"
 head(logret)
 tail(logret)
 
+# Data exploration and save plot
+# png(file= "images/eth_exploration.png", width=550, height=550)
 par(mfrow = c(3,1))
-ts.plot(logret)
-acfPlot(logret)
-pacfPlot(logret)
+plot(ETH$`ETH-USD.Close`, col = 1, main = "ETH/USD", lwd = 0.7)
+plot(log(ETH$`ETH-USD.Close`), col = 1, main = "Logarithmic ETH/USD", lwd = 0.7)
+plot(logret, col = 1, main = "Logarithmic Returns ETH/USD", lwd = 0.7)
+# dev.off()
+
+# Check dependency structure and save plot
+# png(file= "images/dependency.png", width=750, height=420)
+par(mfrow = c(1,2))
+acf(logret, main = "ACF")
+pacf(logret, main = "PACF")
+# dev.off()
+
+# Find best ARIMA-model using auto.arima function from forecast package
+fit <- auto.arima(logret, ic = "bic")
+plot(forecast(fit))
+tsdiag(fit) # looks tip top apart from vola clustering 
 
 # Define in-sample (6m) and out-of-sample (1m)
 
@@ -38,12 +53,30 @@ in_out_sample_separator <- "2016-02-01"
 data_obj <- data_function(logret, c(1:7), in_out_sample_separator)
 head(data_obj$data_mat)
 
-# Fit neural net 
+# Define parameters for fitting RNN
 network_type <- "rnn"
-neurons <- c(7,7,7)
+neurons <- c(14,14,14)
 anz_real <- 5
 period_sharpe <- 365
 epochs <- 40
 
+# Fit RNN using rnn package #####
+# network_type <- "rnn"
+# neurons <- c(14,14,14)
+# anz_real <- 5
+# period_sharpe <- 365
+# epochs <- 40
+
 RNN_predict_obj <- rnn_recurrent_predict(data_obj,neurons, period_sharpe, epochs, network_type)
+RNN_predict_obj$MSE_mat
+
+# Fit RNN using keras package #####
+# neurons <- c(14,14,14)
+# anz_real<- 5
+# period_sharpe<-365
+# epochs<- 40
+
+keras_recurrent_lstm_predict.obj<-keras_recurrent_lstm_predict(data_obj,neurons,period_sharpe,epochs,anz_real)
+keras_recurrent_lstm_predict.obj$MSE_mat
+
 
